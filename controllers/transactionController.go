9b3d4e594ba4cd97510bec77747c6d47e29d64da
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-09-23 18:34:42 +0100

    chore: transfer_funds

--- controllers/transactionController.go
+++ controllers/transactionController.go
@@ -7,42 +7,54 @@ import (
 	"github.com/google/uuid"
 )
 
-func TransferFund(c *fiber.Ctx) error { 
-  wallet_number := c.Params("wallet_number")
-  var wallet Models.Wallet
-
-  senderWallet := db.DB.Find(&wallet, "wallet_number=?", wallet_number)
-  if senderWallet.Error != nil {
-    return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-      "error": "Wallet number not found",
-    })
-  }
+func TransferFund(c *fiber.Ctx) error {
+  senderWalletNumber := c.Params("wallet_number")
   
-  transaction := &Models.Transaction{
+  var senderWallet Models.Wallet
+  if err := db.DB.Find(&senderWallet, "wallet_number=?", senderWalletNumber).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Sender's wallet not found",
+		})
+	}
+
+  var transaction Models.Transaction 
+  if err := c.BodyParser(&transaction); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request format",
+		})
+	}
+
+  if senderWalletNumber == transaction.ReceiverID {
+    return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   "Invalid transaction",
+			"message": "Cannot transfer to yourself",
+		})
+  }
+
+  // Check if the receiver's wallet exists
+  var receiverWallet Models.Wallet
+  if err := db.DB.Find(&receiverWallet, "wallet_number=?", transaction.ReceiverID).Error; err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Receiver's wallet number not found",
+		})
+	}
+
+  if senderWallet.Balance < transaction.Amount {
+    return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   "Insufficient funds",
+			"message": "Sender does not have enough balance for the transfer",
+		})
+  }
+
+  transaction = Models.Transaction{
     ID: uuid.New(),
-    SenderID: wallet_number,
+    SenderID: senderWalletNumber,
+    ReceiverID: transaction.ReceiverID,
+    Amount: transaction.Amount,
   }
   
-  receiverWallet := db.DB.Find(&wallet, "wallet_number=?", transaction.ReceiverID)
-  if receiverWallet.Error != nil {
-    return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-      "error": "Receiver's wallet number not found",
-    })
-  }
-
-  if transaction.SenderID == transaction.ReceiverID {
-    return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-      "error": "Invalid transaction",
-      "message": "Cannot transfer to yourself",
-    })
-  }
-
-  result := db.DB.Save(&transaction)
-  if result.Error != nil {
-    return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-      "error": "Failed transaction",
-    })
-  }
+  senderWallet.Balance -= transaction.Amount
+	receiverWallet.Balance += transaction.Amount
 
   return c.Status(fiber.StatusCreated).JSON(transaction)
 }
--- models/transcationModel.go
+++ models/transcationModel.go
@@ -18,6 +18,15 @@ type Transaction struct {
 }
 
 
+type TranscationResponse struct {
+  ID  uuid.UUID `json:"id,omitempty"`
+	SenderID  string `json:"sender_id,omitempty"`
+	ReceiverID  string `json:"receiver_id,omitempty"`
+  Amount float64 `json:"amount,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 func (Transaction) TableName() string {
   return "transactions"
 }
